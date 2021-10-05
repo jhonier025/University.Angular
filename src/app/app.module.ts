91879e0117c65e4a1ce552eaea9f88a7f4e3ee2b
= <=>
2021-10-04 21:22:49 -0500

    CLASE 2021-10-04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+ ///imports
+import { HttpClientModule} from'@angular/common/http';
+
+/// services provider
+import {StudentService} from './service/student.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './component/student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+   StudentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/domain/student.ts
@@ -0,0 +1,11 @@
+export class Student {
+    constructor(
+        public ID: number,
+        public LastName: string,
+        public FirstMidName: string,
+        public Enrollmentdate: Date,
+        public FullName: string
+
+    ){}
+}
+
